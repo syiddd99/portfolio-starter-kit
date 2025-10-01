a70baf0bea6b2d404806976300edd8d46b5668f0
syiddd99 <m.rasyid.k2@gmail.com>
2025-10-01 11:32:13 +0700

    edit gambar dan teks

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,17 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
+      <main>
+      <h1>Halo — saya [Muchammad Rasyid Syafiyullah]</h1>
+      <p>Ini website sederhana yang saya edit sendiri.</p>
+      <img src="/hero.jpg" alt="Hero" />
+      </main>
+
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+        {`I'm a Muchammad Rasyid Syafiyullah and tab advocate, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
