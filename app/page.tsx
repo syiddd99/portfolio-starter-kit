fa57083fd22ced69d7e00a337b0cd29b04cfc601
regaa-cloud <regabussines@gmail.com>
2025-10-02 14:02:11 +0700

    nama rega

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Page() {
       <main>
       <h1>Welome to OUR WEBSITE</h1>
       <h2>Halo nama saya Muchammad Rasyid Syafiyullah</h2>
+      <h2>dan saya Regata Delavin Abirama</h2>
       <p>Ini website sederhana yang saya edit sendiri.</p>
       <img className='w-40 h-auto' src="file:///C:/Users/ASUS/Downloads/WhatsApp%20Image%202025-10-02%20at%2013.37.02.jpeg" alt="Jari3" />
       </main>
@@ -14,7 +15,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Muchammad Rasyid Syafiyullah and tab advocate, finding unmatched efficiency in
+        {`I'm a Student and tab advocate, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
